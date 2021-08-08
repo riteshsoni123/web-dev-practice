1f6187a6fff0e2877db6190be717477720345614
riteshsoni123 <comedybuddy1@gmail.com>
2021-08-08 12:12:41 +0530

    added the feture to add the link in the DOM

--- web projects/POI_website/script.js
+++ web projects/POI_website/script.js
@@ -16,6 +16,81 @@
 
 
 
+var button = document.querySelector('.btn');
+var inputvalue = document.querySelector('.inputvalue');
+
+btn.addEventListener('click', function () {
+    let clintid = "L2LEFRWKXUWBUYFATD3B1KZHYSEWT1TKLL3ARBVK04EXFPEW";
+    let clintsecret = "3JNNOQPCFFCPVECEQBRHW0XBVOLAKW25CSVARSNOEJJOD4FY";
+
+    fetch(`https://api.foursquare.com/v2/venues/explore?cat=arts&mode=url&near=${inputvalue.value}&client_id=${clintid}&client_secret=${clintsecret}&v=20210101`
+    ).then(resp => resp.json())
+        .then(result => {
+            console.log(result);
+            var length = result['response']['groups'][0]['items']['length'];
+
+            console.log(Number(length));
+            for (let i = 0; i < 1; i++) {
+                // console.log(result['response']['groups'][0]['items'][i]['venue']['categories'][0]['name']);
+                // console.log("bazinga");
+
+
+                let tmp = result['response']['groups'][0]['items'][i]['venue'];
+                let address = tmp['location']['formattedAddress'];
+                let id = tmp['id'];
+
+
+                fetch(`https://api.foursquare.com/v2/venues/${id}/photos?&client_id=${clintid}&client_secret=${clintsecret}&v=20210101`
+                ).then(resp => resp.json())
+                    .then(photo => {
+                        console.log(photo);
+                        let link = '';
+                        // photo['response']['photos']['items'][0]['source']
+                        console.log(photo['response']['photos']['items'][0]['source']);
+                        let source = photo['response']['photos']['items'][0];
+                        let prefix = source['prefix'];;
+                        let suffix = source['suffix'];;
+                        link = `${prefix}300x300${suffix}`;
+                        console.log(link);
+
+
+                        let container=document.querySelector("section");
+                        let div=document.createElement("div");
+                        div.appendChild(document.createTextNode(link));
+                        container.appendChild(div);
+
+                    });
+
+            }
+
+            // let tmp = result['response']['groups'][0]['items'][0]['venue'];
+            // let address = tmp['location']['formattedAddress'];
+            // let id = tmp['id'];
+            // console.log(address);
+            // console.log(id);
+
+
+
+
+
+            // fetch(`https://api.foursquare.com/v2/venues/${id}/photos?&client_id=${clintid}&client_secret=${clintsecret}&v=20210101`
+            // ).then(resp => resp.json())
+            // .then(photo => {
+            //     console.log(photo);
+            //     let link='';
+            //     // photo['response']['photos']['items'][0]['source']
+            //     console.log(photo['response']['photos']['items'][0]['source']);
+            //     let source=photo['response']['photos']['items'][0];
+            //     let prefix=source['prefix'];;
+            //     let suffix=source['suffix'];;
+            //     link=`${prefix}300x300${suffix}`;
+            //     console.log(link);
+
+            // });
+
+        });
+});
+
 
 
 
@@ -24,69 +99,69 @@
 
 
 ///this is to extract point of interest in the city
-let clintid = "L2LEFRWKXUWBUYFATD3B1KZHYSEWT1TKLL3ARBVK04EXFPEW";
-let clintsecret = "3JNNOQPCFFCPVECEQBRHW0XBVOLAKW25CSVARSNOEJJOD4FY";
+// let clintid = "L2LEFRWKXUWBUYFATD3B1KZHYSEWT1TKLL3ARBVK04EXFPEW";
+// let clintsecret = "3JNNOQPCFFCPVECEQBRHW0XBVOLAKW25CSVARSNOEJJOD4FY";
 
-fetch(`https://api.foursquare.com/v2/venues/explore?cat=arts&mode=url&near=kolkata&client_id=${clintid}&client_secret=${clintsecret}&v=20210101`
-).then(resp => resp.json())
-    .then(result => {
-        console.log(result);
-        var length = result['response']['groups'][0]['items']['length'];
+// fetch(`https://api.foursquare.com/v2/venues/explore?cat=arts&mode=url&near=kolkata&client_id=${clintid}&client_secret=${clintsecret}&v=20210101`
+// ).then(resp => resp.json())
+//     .then(result => {
+//         console.log(result);
+//         var length = result['response']['groups'][0]['items']['length'];
 
-        console.log(Number(length));
-        for (let i = 0; i < 5; i++) {
-            // console.log(result['response']['groups'][0]['items'][i]['venue']['categories'][0]['name']);
-            // console.log("bazinga");
-
-
-            let tmp = result['response']['groups'][0]['items'][i]['venue'];
-            let address = tmp['location']['formattedAddress'];
-            let id = tmp['id'];
+//         console.log(Number(length));
+//         for (let i = 0; i < 5; i++) {
+//             // console.log(result['response']['groups'][0]['items'][i]['venue']['categories'][0]['name']);
+//             // console.log("bazinga");
 
 
-            fetch(`https://api.foursquare.com/v2/venues/${id}/photos?&client_id=${clintid}&client_secret=${clintsecret}&v=20210101`
-            ).then(resp => resp.json())
-                .then(photo => {
-                    console.log(photo);
-                    let link = '';
-                    // photo['response']['photos']['items'][0]['source']
-                    console.log(photo['response']['photos']['items'][0]['source']);
-                    let source = photo['response']['photos']['items'][0];
-                    let prefix = source['prefix'];;
-                    let suffix = source['suffix'];;
-                    link = `${prefix}300x300${suffix}`;
-                    console.log(link);
-
-                });
-
-        }
-
-        // let tmp = result['response']['groups'][0]['items'][0]['venue'];
-        // let address = tmp['location']['formattedAddress'];
-        // let id = tmp['id'];
-        // console.log(address);
-        // console.log(id);
+//             let tmp = result['response']['groups'][0]['items'][i]['venue'];
+//             let address = tmp['location']['formattedAddress'];
+//             let id = tmp['id'];
 
 
+//             fetch(`https://api.foursquare.com/v2/venues/${id}/photos?&client_id=${clintid}&client_secret=${clintsecret}&v=20210101`
+//             ).then(resp => resp.json())
+//                 .then(photo => {
+//                     console.log(photo);
+//                     let link = '';
+//                     // photo['response']['photos']['items'][0]['source']
+//                     console.log(photo['response']['photos']['items'][0]['source']);
+//                     let source = photo['response']['photos']['items'][0];
+//                     let prefix = source['prefix'];;
+//                     let suffix = source['suffix'];;
+//                     link = `${prefix}300x300${suffix}`;
+//                     console.log(link);
+
+//                 });
+
+//         }
+
+//         // let tmp = result['response']['groups'][0]['items'][0]['venue'];
+//         // let address = tmp['location']['formattedAddress'];
+//         // let id = tmp['id'];
+//         // console.log(address);
+//         // console.log(id);
 
 
 
-        // fetch(`https://api.foursquare.com/v2/venues/${id}/photos?&client_id=${clintid}&client_secret=${clintsecret}&v=20210101`
-        // ).then(resp => resp.json())
-        // .then(photo => {
-        //     console.log(photo);
-        //     let link='';
-        //     // photo['response']['photos']['items'][0]['source']
-        //     console.log(photo['response']['photos']['items'][0]['source']);
-        //     let source=photo['response']['photos']['items'][0];
-        //     let prefix=source['prefix'];;
-        //     let suffix=source['suffix'];;
-        //     link=`${prefix}300x300${suffix}`;
-        //     console.log(link);
 
-        // });
 
-    });
+//         // fetch(`https://api.foursquare.com/v2/venues/${id}/photos?&client_id=${clintid}&client_secret=${clintsecret}&v=20210101`
+//         // ).then(resp => resp.json())
+//         // .then(photo => {
+//         //     console.log(photo);
+//         //     let link='';
+//         //     // photo['response']['photos']['items'][0]['source']
+//         //     console.log(photo['response']['photos']['items'][0]['source']);
+//         //     let source=photo['response']['photos']['items'][0];
+//         //     let prefix=source['prefix'];;
+//         //     let suffix=source['suffix'];;
+//         //     link=`${prefix}300x300${suffix}`;
+//         //     console.log(link);
+
+//         // });
+
+//     });
 
 
 
