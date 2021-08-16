6a26fe09a263a293aa355dffdc3998dcea360af3
riteshsoni123 <comedybuddy1@gmail.com>
2021-08-16 22:37:35 +0530

    made improvements in calling flights

--- web projects/POI_website/flight.js
+++ web projects/POI_website/flight.js
@@ -1,11 +1,31 @@
-fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/IN/INR/IN/IN/US/2021-08-21?inboundpartialdate=2019-12-01", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "05dce523ffmshb8fc17a05f8c86ap156854jsn4d098c6ba72f",
-		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-	}
-})
-.then(resp=>resp.json())
-.then(result=>{
-    console.log(result);
+
+
+
+var button = document.querySelector('.btn');
+var origin = document.querySelector('.origin');
+var destination = document.querySelector('.destination');
+
+
+
+
+
+btn.addEventListener('click', function () {
+
+	fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/${origin.value}/INR/${origin.value}/${origin.value}/${destination.value}/2021-08-21?inboundpartialdate=2021-08-21`, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "05dce523ffmshb8fc17a05f8c86ap156854jsn4d098c6ba72f",
+			"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+		}
+	})
+		.then(resp => resp.json())
+		.then(result => {
+			console.log(result);
+		});
+
 });
+
+
+
+
+
